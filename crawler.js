339ee69343b65783692b6796a7ad12ab32923d92
fjkk <fujikake.takayoshi@gmail.com>
2023-03-18 18:13:55 +0900

    time str func changed

--- crawler.js
+++ crawler.js
@@ -8,29 +8,9 @@ const puppeteer = require('puppeteer');
 //日付から文字列に変換する関数
 function getYmdHis() {
     let date = new Date();
-    let year_str = date.getFullYear();
-    //月だけ+1すること
-    let month_str = 1 + date.getMonth();
-    let day_str = date.getDate();
-    let hour_str = date.getHours();
-    let minute_str = date.getMinutes();
-    let second_str = date.getSeconds();
-    
-    month_str = ('0' + month_str).slice(-2);
-    day_str = ('0' + day_str).slice(-2);
-    hour_str = ('0' + hour_str).slice(-2);
-    minute_str = ('0' + minute_str).slice(-2);
-    second_str = ('0' + second_str).slice(-2);
-    
-    format_str = 'YYYY-MM-DD hh:mm:ss';
-    format_str = format_str.replace(/YYYY/g, year_str);
-    format_str = format_str.replace(/MM/g, month_str);
-    format_str = format_str.replace(/DD/g, day_str);
-    format_str = format_str.replace(/hh/g, hour_str);
-    format_str = format_str.replace(/mm/g, minute_str);
-    format_str = format_str.replace(/ss/g, second_str);
-    
-    return format_str;
+    date.setTime(date.getTime() + (9*60*60*1000));
+    let str_date = date.toISOString().replace('T', ' ').substr(0, 19);
+    return str_date;
 };    
 
 
